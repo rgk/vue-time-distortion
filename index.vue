6e87ff71cf58540500a0436df23809675bba0524
Robert Kowalski <rgkmg25@gmail.com>
2022-04-20 23:42:53 -0400

    Update index.vue

--- index.vue
+++ index.vue
@@ -1,10 +1,25 @@
 <script setup>
-  defineProps({
-    format: String,
-    style: Number
-  });
+import { ref, defineProps, onMounted, onUnmounted } from 'vue';
+
+defineProps({
+  format: String,
+  style: Number
+});
+
+let time = ref(Date.now());
+
+let intervalId;
+
+onMounted(() => {
+  intervalId = setInterval(() => {
+    time.value = Date.now();
+  }, 1000);
+});
+
+onUnmounted(() => clearInterval(intervalId));
 </script>
 
 <template>
+  {{ time }}
   <slot></slot>
 </template>
